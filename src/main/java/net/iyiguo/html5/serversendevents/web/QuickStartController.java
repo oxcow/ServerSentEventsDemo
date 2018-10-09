5244a86021979fbb3b1cf7e53df4f90759e7cee9
William.li <William.li@activenetwork.com>
2018-10-09 14:04:46 +0800

    使用SseEmitter重写Quick Start 服务端代码

--- src/main/java/net/iyiguo/html5/serversendevents/web/QuickStartController.java
+++ src/main/java/net/iyiguo/html5/serversendevents/web/QuickStartController.java
@@ -2,14 +2,18 @@ package net.iyiguo.html5.serversendevents.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Controller
 @RequestMapping("/html5/sse")
@@ -20,9 +24,9 @@ public class QuickStartController {
     /**
      * 1. 设置response header and encoding
      * 2. 推送指定次数的数据后，服务器主动关闭链接。
-     *
+     * <p>
      * 服务器主动关闭后，浏览器会等待默认时间后重连服务器。一般默认时间是3秒。
-     *
+     * <p>
      * 可以通过 retry 属性设置浏览器重连时间
      *
      * @param req
@@ -45,7 +49,7 @@ public class QuickStartController {
         // writer.flush();
 
         // 推送数据
-        for (int i = 0; i < 5; i++) {
+        for (int i = 1; i < 6; i++) {
             writer.write("data: " + i + ", hello for server send event！" + LocalDateTime.now() + "\n\n");
             writer.flush();
 
@@ -55,7 +59,39 @@ public class QuickStartController {
 
             }
         }
-
         writer.close();
+    }
+
+
+    /**
+     * 浏览器主动关闭或刷新时会服务器会抛出异常。但这是正常的。
+     * https://github.com/codecentric/spring-boot-admin/issues/714
+     *
+     * @return
+     * @see <a href="https://github.com/codecentric/spring-boot-admin/issues/714">java.io.IOException: Broken pipe #714</a>
+     */
+    @RequestMapping("/quick_start_easy")
+    public SseEmitter quickStartEasy() {
+        LOGGER.info("request quick start easy demo......");
+        final SseEmitter sseEmitter = new SseEmitter();
+
+        ExecutorService service = Executors.newSingleThreadExecutor();
+
+        service.execute(() -> {
+            try {
+
+                for (int i = 1; i < 6; i++) {
+                    sseEmitter.send(i + ", hello for server send event!" + LocalDateTime.now(), MediaType.TEXT_PLAIN);
+                    Thread.sleep(2000);
+                }
+
+                sseEmitter.complete();
+
+            } catch (Exception e) {
+                sseEmitter.completeWithError(e);
+            }
+        });
+
+        return sseEmitter;
     }
 }
--- src/main/resources/templates/index.ftl
+++ src/main/resources/templates/index.ftl
@@ -8,6 +8,7 @@
 <h1>Html5 Server Send Event Demo</h1>
 <ul>
     <li><a href="/quick_start.html">快速入门</a></li>
+    <li><a href="/quick_start_easy.html">快速入门(SseEmitter)</a></li>
     <li><a href="/event_fields.html">事件属性测试</a></li>
     <li><a href="/customer_event.html">自定义事件</a></li>
     <li><a href="/mixed_event.html">混合型消息</a></li>
